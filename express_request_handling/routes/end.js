eea8506e330c38612d1393b06d435d5e3ef68174
JWahed <jak.clev@gmail.com>
2022-08-19 12:04:27 +0100

    Update end.js
    renamed constant e to err for clarity

--- express_request_handling/routes/end.js
+++ express_request_handling/routes/end.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 // all unspecified endpoints
 router.use('*', (req, res, next) => {
-  const e = {
+  const err = {
     status: 404,
     message: 'Wrong path in url',
   };
-  const result = res.status(e.status).send(e.message);
+  const result = res.status(err.status).send(err.message);
   return next(result);
 });
 
