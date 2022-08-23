47d8b76eeb22dd6dbf548eeb569913ee571e66f7
JWahed <jak.clev@gmail.com>
2022-08-23 10:06:49 +0100

    Update App.js
    implemented login component

--- react_practise/src/App.js
+++ react_practise/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import PersonInfo from './components/PersonInfo';
 import ConmponentWithProps from './components/ComponentWithProps';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 id="info">
           <PersonInfo forename="Fake" surname="Name" line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
-          <ConmponentWithProps headerProp="hi"/>
+          <ConmponentWithProps/>
+          <Login></Login>
         </h1>
       </header>
     </div>
