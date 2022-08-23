aa32c8cae3e4782af6f3bf16aa84b1a7ccb43a65
JWahed <jak.clev@gmail.com>
2022-08-23 10:12:10 +0100

    Create car.jsx
    Skeleton created

---
+++ react_practise/src/components/car.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Car = () => {
+
+
+    const printCar = event => {
+
+    };
+
+    return(
+        <>
+        
+        </>
+    );
+};
+
+export default Car;
