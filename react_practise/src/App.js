2ed1f87d2b8ff40734e37727e860eb23e8773c0a
JWahed <jak.clev@gmail.com>
2022-08-23 13:50:05 +0100

    Counter

--- react_practise/src/App.js
+++ react_practise/src/App.js
@@ -4,6 +4,8 @@ import PersonInfo from './components/PersonInfo';
 import ConmponentWithProps from './components/ComponentWithProps';
 import Login from './components/Login';
 import Car from './components/Car';
+import ProductTable from './components/ProductTable';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -11,10 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 id="info">
-          <PersonInfo forename="Fake" surname="Name" line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
+          {/* <PersonInfo forename="Fake" surname="Name" line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
           <ConmponentWithProps/>
           <Login/>
-          <Car/>
+          <Car/> */}
+          {/* <ProductTable/> */}
+          <Counter/>
         </h1>
       </header>
     </div>
---
+++ react_practise/src/components/Counter.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Counter = () => {
+
+    const [counter, setCounter] = useState(0);
+    const [previousActions, setPreviousActions] = useState([]);
+
+    const upFive = () => {setCounter(count => count + 5)};
+    const upOne = () => setCounter(count => count + 1);
+    const reset = () => setCounter(0);
+    const downOne = () => setCounter(count => count - 1);
+    const downFive = () => setCounter(count => count - 5);
+
+    const saveActions = () => {setPreviousActions((prev) => [...prev, counter])};
+
+    return(
+        <>
+            <h1>Count: {counter}</h1>
+            
+            <button onClick={() => {upFive(); saveActions();}}> +5 </button>
+            <button onClick={() => {upOne(); saveActions();}}> +1 </button>
+            <button onClick={() => {reset(); saveActions();}}> reset </button>
+            <button onClick={() => {downOne(); saveActions();}}> -1 </button>
+            <button onClick={() => {downFive(); saveActions();}}> -5 </button>
+
+            
+            <p>History</p>
+            <ul>
+                {previousActions.map((counter, index) => (
+                    <li key={index}>{counter}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default Counter;
