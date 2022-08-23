4532840fb1f6ae0db02148a1ed05c3428c4192eb
JWahed <jak.clev@gmail.com>
2022-08-23 16:39:33 +0100

    idek

--- react_practise/src/App.jsx
+++ react_practise/src/App.jsx
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import PersonInfo from './components/PersonInfo';
-import ConmponentWithProps from './components/ComponentWithProps';
-import Login from './components/Login';
-import Car from './components/Car';
+// import PersonInfo from './components/PersonInfo';
+// import ConmponentWithProps from './components/ComponentWithProps';
+// import Login from './components/Login';
+// import Car from './components/Car';
 import ProductTable from './components/ProductTable';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 
 function App() {
   return (
@@ -13,11 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 id="info">
-          {/* <PersonInfo forename="Fake" surname="Name" line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
+          {/* <PersonInfo forename="Fake" surname="Name"
+           line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
           <ConmponentWithProps/>
           <Login/>
           <Car/> */}
-          <ProductTable/>
+          <ProductTable />
           {/* <Counter/> */}
         </h1>
       </header>
