56d79a12565bd3ed01ecfb78c44e5481e0dd3ae4
JWahed <jak.clev@gmail.com>
2022-08-23 16:41:15 +0100

    Update ProductTable.jsx
    Removed unused label and made filter into separate component

--- react_practise/src/components/ProductTable.jsx
+++ react_practise/src/components/ProductTable.jsx
@@ -1,29 +1,27 @@
-import { useState } from "react";
-import Products from "../data/Products.json";
+import { useState } from 'react';
+import Products from '../data/Products.json';
+import contains from './Valid';
 
+function ProductTable() {
+  const [search, setSearch] = useState('');
 
-const ProductTable = () => {
-
-    const [search, setSearch] = useState("");
-
-    return(
-        <>
-            <label htmlFor=""></label>
-            <input type="text" name="searchBox" placeholder="search here" value={search} onChange={(e) => setSearch(e.target.value)}></input>
-            <br/>
-            <ul>
-                {Products.filter((prod) => 
-                    prod.category.toLowerCase().includes(search) ||
-                    prod.flavour.toLowerCase().includes(search) ||
-                    prod.cost.toString().includes(Number.parseFloat(search))
-                ).map((prod) => 
-                    <li key={prod.id}>
-                        {prod.flavour}, {prod.category}, {prod.cost}
-                    </li>
-                )}
-            </ul>
-        </>
-    );
-};
+  return (
+    <>
+      <input type="text" name="searchBox" placeholder="search here" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <br />
+      <ul>
+        {Products.filter((prod) => contains(prod, search)).map((prod) => (
+          <li key={prod.id}>
+            {prod.flavour}
+            ,
+            {prod.category}
+            ,
+            {prod.cost}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default ProductTable;
