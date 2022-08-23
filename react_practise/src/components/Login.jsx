025705bbaff0b148a655b80efe312010acc177a9
JWahed <jak.clev@gmail.com>
2022-08-23 10:06:35 +0100

    Update Login.jsx
    implemented login field

--- react_practise/src/components/Login.jsx
+++ react_practise/src/components/Login.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const Login = () => {
+
+    const [user, setUser] = useState("username");
+    const [pass, setPass] = useState("password");
 
     return(
         <>
-
+            <form>
+                <label>Username:</label>
+                <input name="username" type="text" value={user} onChange={(event) => setUser(event.target.value)}></input>
+                <label>Password:</label>
+                <input name="password" type="text" value={pass} onChange={(event) => setPass(event.target.value)}></input>
+                <br/>
+                <button>Submit</button>
+            </form>
         </>
     );
 };
