c8c0eb88fbf5c672a04d1a648757aece5733cd63
JWahed <jak.clev@gmail.com>
2022-08-23 10:35:43 +0100

    Update car.jsx
    implemented car info

--- react_practise/src/components/car.jsx
+++ react_practise/src/components/car.jsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 
 const Car = () => {
 
-
-    const printCar = event => {
-
-    };
+    const [brand, setBrand] = useState("Your");
+    const [model, setModel] = useState("Car");
+    const [colour, setColour] = useState("Specs");
+    const [year, setYear] = useState("Here");
 
     return(
         <>
-        
+            <h1>Specs: {brand}, {model}, {colour}, {year}</h1>
+            
+            <form>
+                <label htmlFor="brand"></label>
+                <input type="text" name="brand" value={brand} onChange={(event) => setBrand(event.target.value)}/>
+
+                <label htmlFor="model"></label>
+                <input type="text" name="model" value={model} onChange={(event) => setModel(event.target.value)}/>
+
+                <label htmlFor="colour"></label>
+                <input type="text" name="colour" value={colour} onChange={(event) => setColour(event.target.value)}/>
+
+                <label htmlFor="year"></label>
+                <input type="text" name="year" value={year} onChange={(event) => setYear(event.target.value)}/>
+                <br/>
+            </form>
         </>
     );
 };
