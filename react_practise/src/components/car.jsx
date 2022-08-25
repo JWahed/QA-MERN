287d67cac4cb0519c163011b25e8aa9ca8f76a34
JWahed <jak.clev@gmail.com>
2022-08-25 16:29:34 +0100

    eslint

--- react_practise/src/components/car.jsx
+++ react_practise/src/components/car.jsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useState } from 'react';
 
-const Car = () => {
+function Car() {
+  const [brand, setBrand] = useState('Your');
+  const [model, setModel] = useState('Car');
+  const [colour, setColour] = useState('Specs');
+  const [year, setYear] = useState('Here');
 
-    const [brand, setBrand] = useState("Your");
-    const [model, setModel] = useState("Car");
-    const [colour, setColour] = useState("Specs");
-    const [year, setYear] = useState("Here");
+  return (
+    <>
+      <h1>
+        Specs:
+        {brand}
+        ,
+        {model}
+        ,
+        {colour}
+        ,
+        {year}
+      </h1>
 
-    return(
-        <>
-            <h1>Specs: {brand}, {model}, {colour}, {year}</h1>
-            
-            <form>
-                <label htmlFor="brand"></label>
-                <input type="text" name="brand" value={brand} onChange={(event) => setBrand(event.target.value)}/>
+      <form>
+        <label htmlFor="brand" />
+        <input type="text" name="brand" value={brand} onChange={(event) => setBrand(event.target.value)} />
 
-                <label htmlFor="model"></label>
-                <input type="text" name="model" value={model} onChange={(event) => setModel(event.target.value)}/>
+        <label htmlFor="model" />
+        <input type="text" name="model" value={model} onChange={(event) => setModel(event.target.value)} />
 
-                <label htmlFor="colour"></label>
-                <input type="text" name="colour" value={colour} onChange={(event) => setColour(event.target.value)}/>
+        <label htmlFor="colour" />
+        <input type="text" name="colour" value={colour} onChange={(event) => setColour(event.target.value)} />
 
-                <label htmlFor="year"></label>
-                <input type="text" name="year" value={year} onChange={(event) => setYear(event.target.value)}/>
-                <br/>
-            </form>
-        </>
-    );
-};
+        <label htmlFor="year" />
+        <input type="text" name="year" value={year} onChange={(event) => setYear(event.target.value)} />
+        <br />
+      </form>
+    </>
+  );
+}
 
 export default Car;
