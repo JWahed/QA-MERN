322984c1f8492df160897bbc91e8ea51fac8d77f
JWahed <jak.clev@gmail.com>
2022-08-25 16:29:18 +0100

    Update App.jsx
    Implemented routes

--- react_practise/src/App.jsx
+++ react_practise/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 // import PersonInfo from './components/PersonInfo';
@@ -7,14 +8,19 @@ import './App.css';
 // import ProductTable from './components/ProductTable';
 // import Counter from './components/Counter';
 // import Cart from './components/basket/Cart';
-import HideText from './components/hooks_exercise/HideText';
+// import HideText from './components/hooks_exercise/HideText';
+import FilmRequest from './components/film_request/FilmRequest ';
+import NotFound from './components/routes_exercise/NotFound';
+import Home from './components/routes_exercise/Home';
+import Contact from './components/routes_exercise/Contact';
+import Users from './components/routes_exercise/Users';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 id="info">
+        <div id="info">
           {/* <PersonInfo forename="Fake" surname="Name"
            line1="House House" line2="4th Street" city="London" postCode="FA1 1KE"/>
           <ConmponentWithProps/>
@@ -23,7 +29,7 @@ function App() {
           {/* <ProductTable /> */}
           {/* <Counter/> */}
           {/* <Cart /> */}
-          <HideText
+          {/* <HideText
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -32,8 +38,17 @@ function App() {
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum."
             end={20}
-          />
-        </h1>
+          /> */}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/users/:id" element={<Users />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/404" element={<NotFound />} />
+              <Route path="/film_request" element={<FilmRequest />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </header>
     </div>
   );
